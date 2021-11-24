69f8958a0759fbf186e1c8ff22317b6315bc7727
Ben Moore-Maley <bmoorema@eoas.ubc.ca>
2021-11-24 13:20:08 -0500

    Added current evaluation postprocessing suite on CC graham

---
+++ notebooks/graham/process_NEMO_drifters.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+
+import numpy as np
+import xarray as xr
+import pandas as pd
+import sqlite3 as sql
+from datetime import datetime, timedelta
+from dateutil.parser import parse
+from scipy import io, interpolate
+import sys
+
+
+def mtime_to_datetime(mtime):
+    """Convert from Matlab time to datetime.datetime
+    """
+    
+    return datetime.fromordinal(int(mtime) - 366) + timedelta(days=mtime%1)
+
+
+def boundingbox(data, time, lon, lat, gridvars, gridref, size=1):
+    """Return a bounding box of coordinate, value pairs around a lonlat point
+    """
+
+    n, m = gridvars['lonlat'][0].shape
+    j, i = [gridref[var].sel(lats=lat, lons=lon, method='nearest').item() for var in ('jj', 'ii')]
+    if any([j < size, i < size, j >= n - size, i >= m - size]): return None, None
+    jslc, islc = [slice(coord - size, coord + size + 1) for coord in (j, i)]
+    points = np.vstack([coord[jslc, islc].ravel() for coord in gridvars['lonlat']]).T
+    values = data.isel(y=jslc, x=islc).interp(time_counter=time).values
+    values = np.ma.masked_where(gridvars['mask'][jslc, islc] == 0, values).ravel()
+    
+    return points, values
+
+
+def get_grid_variables(path='/home/bmoorema/MEOPAR/grid/'):
+    """Return grid variables dict GRIDVARS with LONLAT and MASK fields for U and V.
+    Also return the GRIDREF xarray object for looking up ji from lonlat
+    """
+
+    # Master files
+    coords = xr.open_dataset(path + 'coordinates_seagrid_SalishSea201702.nc', decode_times=False)
+    mask = xr.open_dataset(path + 'mesh_mask202108.nc')
+    gridref = xr.open_dataset(path + 'grid_from_lat_lon_mask999.nc')
+
+    # Grid variables
+    gridvars = {'u': {}, 'v': {}}
+    for var in ['u', 'v']:
+        gridvars[var]['lonlat'] = [coords[key][0, ...].values for key in (f'glam{var}', f'gphi{var}')]
+        gridvars[var]['mask'] = mask[f'{var}mask'][0, 0, ...].values
+    
+    return gridvars, gridref
+
+
+def get_drifter_IDs(daterange, drifters):
+    """Return drifter IDs and unique observation times on the half-hour
+    within DATERANGE
+    """
+    
+    
+    # Get IDs list
+    start, end = [parse(date) for date in daterange]
+    IDs, IDout = list(np.unique(drifters['id'].astype(int))), []
+    for ID in IDs:
+        dindex = np.where(drifters['id'].astype(int) == ID)[0][0]
+        if drifters['lon'][dindex][0] < -124.5: IDout.append(ID)
+        if not all(start <= mtime_to_datetime(t) <= end for t in drifters['mtime'][dindex][:, 0]): IDout.append(ID)
+    IDout = list(np.unique(IDout))
+    for ID in IDout: IDs.remove(ID)
+
+    # Get inclusive times on the half-hour
+    times, kwargs = [], {'minute': 30, 'second': 0, 'microsecond': 0}
+    for ID in IDs:
+        dindex = np.where(drifters['id'].astype(int) == ID)[0][0]
+        for t in drifters['mtime'][dindex][:, 0]:
+            for hour in range(-1, 2):
+                times.append((mtime_to_datetime(t) + timedelta(hours=hour)).replace(**kwargs))
+    
+    return IDs, np.unique(times)
+
+
+def get_NEMO_velocities(ID, drifters, NEMO, gridvars, gridref):
+    """Interpolate NEMO velocites at times and positions based on drifter ID
+    and return dict of arrays
+    """
+    
+    # Load drifters
+    dindex = np.where(drifters['id'].astype(int) == ID)[0][0]
+    times = np.array([mtime_to_datetime(t) for t in drifters['mtime'][dindex][:, 0]])
+    lons, lats = [drifters[key][dindex][:, 0] for key in ('lon', 'lat')]
+    
+    # Load NEMO
+    vel = {'u': [], 'v': []}
+    for time, lon, lat in zip(times, lons, lats):
+        for var in ['u', 'v']:
+            points, values = boundingbox(NEMO[var], time, lon, lat, gridvars[var], gridref)
+            val = interpolate.griddata(points, values, (lon, lat)) if points is not None else np.nan
+            vel[var].append(float(val))
+    
+    return {'time': times, 'longitude': lons, 'latitude': lats, 'u': np.array(vel['u']), 'v': np.array(vel['v'])}
+
+
+def main_routine(
+    runID, startdate, enddate,
+    path_NEMO='/scratch/bmoorema/Results/Currents',
+    path_obs='/home/bmoorema/project/SalishSea/obs',
+    path_save='/scratch/bmoorema/Drifters',
+
+):
+    """Process NEMO velocities from runID onto drifter trajectories for STARTDATE, ENDDATE
+    (STARTDATE, ENDDATE must match the results file path)
+    """
+
+    # Initialize
+    daterange = [startdate, enddate]
+    datestr = '_'.join(daterange)
+    sys.stdout = open(f'{path_save}/stdout/SSCdrifters_{runID}_{datestr}.stdout', 'w')
+    rundir = f'SalishSeaCast_currenttuning_{runID}_{datestr}'
+    print('Processing results from ' + rundir)
+    sys.stdout.flush()
+    
+    # Load drifters
+    drifters = io.loadmat(f'{path_obs}/Salish_L3_20190728T103529.mat')['drift'][0]
+    IDs, times = get_drifter_IDs(daterange, drifters)
+
+    # Load NEMO
+    NEMO = {}
+    for var, name in zip(['u', 'v'], ['vozocrtx', 'vomecrty']):
+        print(f'Loading {var} ...')
+        sys.stdout.flush()
+        fn = f'{path_NEMO}/{rundir}/SalishSea_1h_{datestr}_grid_{var.upper()}.nc'
+        NEMO[var] = xr.open_dataset(fn)[name].isel({f'depth{var}': 0}).sel(time_counter=times).load()
+
+    # Get velocities and save to database
+    con = sql.connect(f'{path_save}/SSCdrifters_{runID}_{datestr}.sqlite')
+    gridvars, gridref = get_grid_variables()
+    for n, ID in enumerate(IDs):
+        print(f'Loading drifter {ID:03d} ({n+1} of {len(IDs)}) ...')
+        sys.stdout.flush()
+        velocities = get_NEMO_velocities(ID, drifters, NEMO, gridvars, gridref)
+        pd.DataFrame(velocities).to_sql(f'drifter{ID:03d}', con, index=False)
+    con.close()
+    print('Done!')
+    sys.stdout.close()
+
+
+if __name__ == "__main__":
+    main_routine(*[str(arg) for arg in sys.argv[1:]])
---
+++ notebooks/graham/process_NEMO_drifters.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --account=rrg-allen
+#SBATCH --job-name="process_NEMO_drifters"
+#SBATCH --time=3:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=9
+#SBATCH --mem-per-cpu=10G
+#SBATCH --mail-user=bmoorema@eoas.ubc.ca
+#SBATCH --mail-type=ALL
+
+module load glost/0.3.1
+module load python/3.9.6
+module load netcdf/4.7.4
+virtualenv --no-download ~/env/
+source ~/env/bin/activate
+python3 -m pip install --no-index --upgrade pip
+python3 -m pip install --no-index numpy bottleneck netCDF4 pandas scipy xarray
+
+echo "Starting run at: `date`"
+srun glost_launch process_NEMO_drifters.list
+echo "Finished run at: `date`"
