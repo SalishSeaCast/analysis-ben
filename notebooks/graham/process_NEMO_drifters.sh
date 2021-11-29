8498c49fe133ba1d4d1a4ab58af3f154d1425fb6
Ben Moore-Maley <bmoorema@eoas.ubc.ca>
2021-11-29 14:00:18 -0500

    Update drifter evaluation in notebooks/graham

--- notebooks/graham/process_NEMO_drifters.sh
+++ notebooks/graham/process_NEMO_drifters.sh
@@ -3,12 +3,12 @@
 #SBATCH --job-name="process_NEMO_drifters"
 #SBATCH --time=3:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=9
-#SBATCH --mem-per-cpu=10G
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=20G
 #SBATCH --mail-user=bmoorema@eoas.ubc.ca
 #SBATCH --mail-type=ALL
 
-module load glost/0.3.1
+#module load glost/0.3.1
 module load python/3.9.6
 module load netcdf/4.7.4
 virtualenv --no-download ~/env/
@@ -17,5 +17,6 @@ python3 -m pip install --no-index --upgrade pip
 python3 -m pip install --no-index numpy bottleneck netCDF4 pandas scipy xarray
 
 echo "Starting run at: `date`"
-srun glost_launch process_NEMO_drifters.list
+srun python3 process_NEMO_drifters.py charnock 20160401 20160930 
+#srun glost_launch process_NEMO_drifters.list
 echo "Finished run at: `date`"
